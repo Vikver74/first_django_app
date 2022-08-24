b67e83356e5ee3ef808406851f796f48c86b5a9f
vikver74@mail.ru <iamber97>
2022-08-24 19:21:58 +0300

    My first Django app 2022.08.24

--- news/models.py
+++ news/models.py
@@ -2,6 +2,9 @@ from django.db import models
 
 
 # Create your models here.
+from django.urls import reverse
+
+
 class News(models.Model):
     title = models.CharField(max_length=200, verbose_name='Заголовок')
     content = models.TextField(blank=True, verbose_name='Контент')
@@ -10,6 +13,9 @@ class News(models.Model):
     photo = models.ImageField(upload_to='photos/%Y/%m/%d',verbose_name='Фото', blank=True)
     is_published = models.BooleanField(default=True, verbose_name='Опубликовано')
     category = models.ForeignKey('Category', on_delete=models.PROTECT, null=True, verbose_name='Категория')
+
+    def get_absolute_url(self):
+        return reverse('view_news', kwargs={'news_id': self.pk})
 
     def __str__(self):
         return self.title
@@ -22,6 +28,9 @@ class News(models.Model):
 
 class Category(models.Model):
     title = models.CharField(max_length=200, verbose_name='Наименование категории', db_index=True)
+
+    def get_absolute_url(self):
+        return reverse('category', kwargs={'category_id': self.pk})
 
     def __str__(self):
         return self.title
--- news/views.py
+++ news/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from .models import News, Category
 
@@ -7,22 +7,26 @@ from .models import News, Category
 
 def index(request):
     news = News.objects.all()
-    categories = Category.objects.all()
     context = {
         'news': news,
         'title': 'Список новостей',
-        'categories': categories,
     }
     return render(request, 'news/index.html', context=context)
 
 
 def get_category(request, category_id):
     news = News.objects.filter(category_id=category_id)
-    categories = Category.objects.all()
     category = Category.objects.get(pk=category_id)
     context = {
         'news': news,
-        'categories': categories,
         'category': category,
     }
     return render(request, 'news/category.html', context=context)
+
+
+def view_news(request, news_id):
+    news_item = get_object_or_404(News, pk=news_id)
+    context = {
+        'news_item': news_item
+    }
+    return render(request, 'news/view_news.html', context=context)

0706e5a5b334850d7a8e10d74e6ed8ccd4086c33
vikver74@mail.ru <iamber97>
2022-09-05 20:37:49 +0300

    version with views as functions

--- news/views.py
+++ news/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse
 from .models import News, Category
+from .forms import NewsForm
+
 
 # Create your views here.
 
@@ -30,3 +32,19 @@ def view_news(request, news_id):
         'news_item': news_item
     }
     return render(request, 'news/view_news.html', context=context)
+
+
+def add_news(request):
+    if request.method == 'POST':
+        form = NewsForm(request.POST)
+        if form.is_valid():
+            # news = News.objects.create(**form.cleaned_data)     #  сохранение данных из не связанной с моделью формы
+            news = form.save()  # сохранение данных из связанной с моделью формы
+            return redirect(news)
+    else:
+        form = NewsForm()
+
+    context = {
+        'form': form
+    }
+    return render(request, 'news/add_news.html', context)
